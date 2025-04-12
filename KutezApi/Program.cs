299771179fad5b23260f25ffde787f56ed9a21fc
Berkay Kara <berkay.kara02@bilgiedu.net>
2025-04-12 18:50:02 +0300

    finish

--- KutezApi/Program.cs
+++ KutezApi/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddScoped<GoldPriceService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var port = Environment.GetEnvironmentVariable("PORT");
+
+if (port != null)
+{
+    builder.WebHost.UseUrls("http://0.0.0.0:" + port);
+}
 
 var app = builder.Build();
 
