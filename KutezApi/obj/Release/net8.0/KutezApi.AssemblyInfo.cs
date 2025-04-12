03ae29b1ac210904faba3095e26ebba06166d271
Berkay Kara <berkay.kara02@bilgiedu.net>
2025-04-12 18:53:01 +0300

    publish

--- KutezApi/obj/Release/net8.0/KutezApi.AssemblyInfo.cs
+++ KutezApi/obj/Release/net8.0/KutezApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KutezApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80dd03d2ff05109392f0799c7daca653a602e1e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299771179fad5b23260f25ffde787f56ed9a21fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("KutezApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KutezApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
